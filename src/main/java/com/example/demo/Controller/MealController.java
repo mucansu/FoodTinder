38331bc33c166f634eefdd33c478fbbb6e2e57a0
RaisaKemppainen <raisa.kemppainen@brightstraining.com>
2023-01-29 16:26:37 +0100

    Lagt till korrigerad kod för att lägga till måltid i SessionMealList. index-1. Samt lagt till if-sats i if-satsen som kollar om listan av måltider är slut. Om sista måltiden är "yes" ska även den läggas till i listan. Lagt till tillfällig paragraf för att veta vilken profil som gör "swipe sessionen". Kan tas bort när applikationen är klar.

--- src/main/java/com/example/demo/Controller/MealController.java
+++ src/main/java/com/example/demo/Controller/MealController.java
@@ -18,91 +18,98 @@ import java.util.stream.Collectors;
 
 @Controller
 public class MealController {
-    @Autowired
-    private MealService mealService;
-    @Autowired
-    private ProfileService profileService;
-    @Autowired
-    private MealRepository mealRepository;
+	@Autowired
+	private MealService mealService;
+	@Autowired
+	private ProfileService profileService;
+	@Autowired
+	private MealRepository mealRepository;
 
 
-    @GetMapping("/main/{id}")
-    public String mainPage(Model model, @PathVariable Long id,
-                           @RequestParam(required = false, defaultValue = "0") Integer index,
-                           @RequestParam(required = false, defaultValue = "default") String choice,
-                           HttpSession session) {
+	@GetMapping("/main/{id}")
+	public String mainPage(Model model, @PathVariable Long id,
+	                       @RequestParam(required = false, defaultValue = "0") Integer index,
+	                       @RequestParam(required = false, defaultValue = "default") String choice,
+	                       HttpSession session) {
 
-        //Kollar vilken profil som är inloggad
-        Profile profile = (Profile) session.getAttribute("profile");
-        if (profile == null || profile.getId() != id) {//kolla om gamla profilen är kvar
-            profile = profileService.findById(id);
-            session.setAttribute("profile", profile);
-        }
-
-
-        List<Meal> mealList = mealService.findAll(); //Tar alla måltider från databas.
-
-        //När mealList är 20 ska resultat visas och profiler som har valt sina YES meals sparas i profileList.
-        if (index >= mealList.size()) {
-            List<Profile> profileList = (List<Profile>) session.getAttribute("profileList");
-            if (profileList == null) {
-                profileList = new ArrayList<>();
-                session.setAttribute("profileList", profileList);
-            }
-            profileList.add(profile);
-            model.addAttribute("yesMealList", profile.getSessionMealList());
-            return "result";
-        }
-        Meal meal = mealList.get(index);//Hämtar index på måltid
-
-        model.addAttribute("mealList", mealList);
-        model.addAttribute("meal", meal);
-        model.addAttribute("mealIndex", index + 1);//Tar nästa måltid
-        if (choice.equals("yes")) {
-            profile.getSessionMealList().add(meal);//Om choice yes läggs måltiden i listan i profiles matlista.
-
-        }
-
-        return "main";
-    }
+		//Kollar vilken profil som är inloggad
+		Profile profile = (Profile) session.getAttribute("profile");
+		if (profile == null || profile.getId() != id) {//kolla om gamla profilen är kvar
+			profile = profileService.findById(id);
+			session.setAttribute("profile", profile);
+		}
 
 
-    @GetMapping("/matchingMeals")
-    public String matchingMeals(Model model, HttpSession session) {
-        //Profile currentProfile = (Profile) session.getAttribute("profile"); //Gets current profile from session
-        List<Profile> profileList = (List<Profile>) session.getAttribute("profileList");// Gets List of Profiles in the session
-        List<Meal> matchingMeals = new ArrayList<>(); //Empty List to add the matchingMeals to
+		List<Meal> mealList = mealService.findAll(); //Tar alla måltider från databas.
 
-        Map<Long, Integer> matchedMeals = new HashMap<>();
-        for (Profile profile : profileList) {
-            for (Meal m : profile.getSessionMealList()) {
-                Long id = m.getId();
-                if (!matchedMeals.containsKey(id)) {
-                    matchedMeals.put(id, 1);
-                } else {
-                    int numberOfPreviousOccurrences = matchedMeals.get(id);
-                    matchedMeals.put(id, numberOfPreviousOccurrences + 1);
-                }
-            }
-        }
+		//När mealList.size är nådd ska resultat visas och profiler som har valt sina YES meals sparas i profileList.
+		if (index >= mealList.size()) {
 
-        List<Long> matchedMealsIds = new ArrayList<>();
-        for (Map.Entry<Long, Integer> entry : matchedMeals.entrySet()) {
-            if (entry.getValue() == profileList.size()) {
-                matchedMealsIds.add(entry.getKey());
-            }
-            if (entry.getValue() == profileList.size() - 1) {
-                matchedMealsIds.add(entry.getKey());
-            }
-        }
+			if (choice.equals("yes")) { //Om sista måltiden är choice "yes".
+				Meal lastmeal = mealList.get(index - 1); //Skapa en lastmeal och sätt den till föregående index.
+				profile.getSessionMealList().add(lastmeal); //Addera den till SessionMealList.
+			}
 
-        List<Meal> mealList = mealService.findAll(); //Tar alla måltider från databas.
-        List<Meal> mealListById = mealList.stream()
-                                           .filter(meal -> matchedMealsIds.contains(meal.getId()))
-                                           .collect(Collectors.toList());
+			List<Profile> profileList = (List<Profile>) session.getAttribute("profileList");
+			if (profileList == null) {
+				profileList = new ArrayList<>();
+				session.setAttribute("profileList", profileList);
+			}
+			profileList.add(profile);
+			model.addAttribute("yesMealList", profile.getSessionMealList());
+			return "result";
+		}
 
-        model.addAttribute("mealListById",mealListById);
-        return "matchingMeals"; //Shows the matchingMeals view.
-    }
+		Meal meal = mealList.get(index);//Hämtar index på måltid
+
+		model.addAttribute("mealList", mealList);
+		model.addAttribute("meal", meal);
+		model.addAttribute("mealIndex", index + 1);//Tar nästa måltid
+
+		if (choice.equals("yes")) {
+			Meal addmeal = mealList.get(index - 1);//Hämtar indexet för måltiden som visades
+			profile.getSessionMealList().add(addmeal);//Om choice yes läggs måltiden i listan i profiles matlistan
+		}
+
+		return "main";
+	}
+
+
+	@GetMapping("/matchingMeals")
+	public String matchingMeals(Model model, HttpSession session) {
+		//Profile currentProfile = (Profile) session.getAttribute("profile"); //Gets current profile from session
+		List<Profile> profileList = (List<Profile>) session.getAttribute("profileList");// Gets List of Profiles in the session
+		List<Meal> matchingMeals = new ArrayList<>(); //Empty List to add the matchingMeals to
+
+		Map<Long, Integer> matchedMeals = new HashMap<>();
+		for (Profile profile : profileList) {
+			for (Meal m : profile.getSessionMealList()) {
+				Long id = m.getId();
+				if (!matchedMeals.containsKey(id)) {
+					matchedMeals.put(id, 1);
+				} else {
+					int numberOfPreviousOccurrences = matchedMeals.get(id);
+					matchedMeals.put(id, numberOfPreviousOccurrences + 1);
+				}
+			}
+		}
+
+		List<Long> matchedMealsIds = new ArrayList<>();
+		for (Map.Entry<Long, Integer> entry : matchedMeals.entrySet()) {
+			if (entry.getValue() == profileList.size()) {
+				matchedMealsIds.add(entry.getKey());
+			} else if (entry.getValue() == profileList.size() - 1) {
+				matchedMealsIds.add(entry.getKey());
+			}
+		}
+
+		List<Meal> mealList = mealService.findAll(); //Tar alla måltider från databas.
+		List<Meal> mealListById = mealList.stream()
+				                          .filter(meal -> matchedMealsIds.contains(meal.getId()))
+				                          .collect(Collectors.toList());
+
+		model.addAttribute("mealListById", mealListById);
+		return "matchingMeals"; //Shows the matchingMeals view.
+	}
 
 }
