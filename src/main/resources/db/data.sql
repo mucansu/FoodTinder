b0978698e8007c9140c06df0b123b87e84083260
Mustafa <mucansu@gmail.com>
2023-09-01 00:04:37 +0200

    DB migration files added

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,22 @@
+use foodalong
+insert into meal(name) values('pizza');
+insert into meal(name) values('Hamburger');
+insert into meal(name) values('Lazania');
+insert into meal(name) values('Rossito');
+insert into meal(name) values('Biryani');
+insert into meal(name) values('Kebab');
+insert into meal(name) values('meat balls');
+insert into meal(name) values('Mac and cheese');
+insert into meal(name) values('Bouritos');
+insert into meal(name) values('Glutin-free pizza');
+insert into meal(name) values('Falafel');
+insert into meal(name) values('Hot Dogs');
+insert into meal(name) values('Salmon');
+insert into meal(name) values('Sushi');
+insert into meal(name) values('chicken tandori');
+insert into meal(name) values('chicken tyaraki');
+insert into meal(name) values('pasta');
+insert into meal(name) values('Carbonara');
+insert into meal(name) values('pasta pomodoro');
+insert into meal(name) values('Noodles with crab');
+select * from meal
